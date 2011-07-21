7402e311c093bffd77c9dee995f7053b2126c89b
xiaobin <julywoaini@163.com>
2011-07-21 11:11:34 +0800

    vim skill 宏定义=> 10000 times
    define HOW_MANY_TIMES_TO_SAY_HELLO 10000
    
     ctrl+n is used in INSERT mode
    
    it will search current file and all included files

--- hello.c
+++ hello.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#define HOW_MANY_TIMES_TO_SAY_HELLO 10000
 
  int main(int argc, const *argvc[])
    {
        int i;
-       for (i=0;i<10;i++)
+       for (i=0;i<HOW_MANY_TIMES_TO_SAY_HELLO;i++)
           { 
              printf("hello world!\n");           
           }
