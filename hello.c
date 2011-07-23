937a089dae5f2909907a9adea5f7dd6d14b98a79
xiaobin <julywoaini@163.com>
2011-07-23 08:46:40 +0800

    coding style
    bed:
    if(){
    
    }
    
    good:
    if()
    {
    
    }

--- hello.c
+++ hello.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #define HOW_MANY_TIMES_TO_SAY_HELLO 10000
 
- int main(int argc, const *argvc[])
-   {
+int main(int argc, const *argvc[])
+{
        int i;
        for (i=0;i<HOW_MANY_TIMES_TO_SAY_HELLO;i++)
-          { 
+       { 
              printf("hello world!\n");           
-          }
+       }
        return  0;
-  }
+}
