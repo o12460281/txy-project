e8eb450a2920a2e360e458932586a87f8fe619bc
xiaobin <julywoaini@163.com>
2011-07-30 17:18:59 +0800

    maybe I should ask teacher for hell

--- 4note/usestr.c
+++ 4note/usestr.c
@@ -4,10 +4,8 @@ int main(int argc, char const* argv[])
 {
     char str[]="julyhello";
     char *p;
-    p = strstr(str,"he");
-    strcpy(*p,"he",2);
-    printf("%s\n",str);
-    //char strstr(*p,a)
-   // printf("%c\n",strstr(*p,a));
+   //? strcpy(str,"he",2);
+   printf("%s\n",strstr(str,"l"));
+   printf("%s\n",strchr(str,'l'));
     return 0;
 }
